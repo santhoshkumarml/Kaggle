376383097cf8c36671c76ee9c4acaeee672afb3b
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-02-14 21:58:30 -0500

    changes

--- Titanic/decision-tree/classify.py
+++ Titanic/decision-tree/classify.py
@@ -6,7 +6,8 @@ Created on Feb 14, 2015
 
 import pandas
 from sklearn.tree import DecisionTreeClassifier
-import csv
+from sklearn import tree
+
 train_data = pandas.read_csv('../train.csv')
 train_data_target = train_data['Survived']
 train_data = train_data.drop('Survived',axis=1)
@@ -15,12 +16,13 @@ test_data = pandas.read_csv('../test.csv')
 cols = list(test_data.columns.values)
 cols.remove('PassengerId')
 
-cl = DecisionTreeClassifier()
 train_data['Sex'] = [1 if row=='male' else 0 for row in train_data['Sex']]
 test_data['Sex'] = [1 if row=='male' else 0 for row in test_data['Sex']]
-  
-cl.fit(train_data[['Sex', 'Pclass', 'Parch', 'SibSp']], train_data_target)
+
+cl = DecisionTreeClassifier()  
+cl_t = cl.fit(train_data[['Sex', 'Pclass', 'Parch', 'SibSp']], train_data_target)
 op = cl.predict(test_data[['Sex', 'Pclass', 'Parch', 'SibSp']])
+
 test_data = test_data.drop(cols, axis=1)
 test_data['Survived'] = op
 test_data.info()
