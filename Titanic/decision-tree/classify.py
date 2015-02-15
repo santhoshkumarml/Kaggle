9dc1dc5a7aefd181da61504d133316947a308e18
Santhosh Kumar Manavasi Lakshminarayanan <Santhosh Kumar Manavasi Lakshminarayanan>
2015-02-15 03:00:15 -0500

    Changes

--- Titanic/decision-tree/classify.py
+++ Titanic/decision-tree/classify.py
@@ -10,7 +10,7 @@ from sklearn import tree
 
 train_data = pandas.read_csv('../train.csv')
 train_data_target = train_data['Survived']
-train_data = train_data.drop('Survived',axis=1)
+train_data = train_data.drop('Survived', axis=1)
 test_data = pandas.read_csv('../test.csv')
 
 cols = list(test_data.columns.values)
